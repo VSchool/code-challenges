e90114ea349982c8890ed586cb25b498f8999282
Bob Ziroll <elderziroll@gmail.com>
2018-10-29 17:13:16 -0600

    Fix formatting issue

--- daily-challenges/no-duplicates/index.test.js
+++ daily-challenges/no-duplicates/index.test.js
@@ -1,13 +1,10 @@
-const {
-    noDuplicates
-} = require("./index")
+const { noDuplicates } = require("./index")
 
 describe("No duplicates", () => {
     test("Duplicates are successfully removed", () => {
         const str = "bookkeeper larry"
         const expected = "bokepr lay"
         const actual = noDuplicates(str)
-        console.log(actual)
         expect(actual).toBe(expected)
     })
     test("Function returns a string", () => {
