9f8fb35c1008fbb7b92a0451d369398246a06020
Eric <ericnatejones@gmail.com>
2018-10-29 16:08:13 -0600

    there is no spoon

--- daily-challenges/there-is-no-spoon/index.js
+++ daily-challenges/there-is-no-spoon/index.js
@@ -1,1 +1,14 @@
-//your function(s) here
+//your objects(s) here
+
+// Create and declare 10 objects from things you see. Name each one with const, let, or var.  
+// Each object should have at least 3 keys (properties).
+// Have at least one boolean, string, number, array, nested object, etc.
+// Each object should have at least one method.
+
+
+
+// list obejects in this array and run tests to test
+
+const objectArray = ["PLACE OBJECTS HERE. Separate objects with commas"];
+
+module.exports = objectArray;
--- daily-challenges/there-is-no-spoon/index.test.js
+++ daily-challenges/there-is-no-spoon/index.test.js
@@ -1,7 +1,39 @@
-const functionsToTest = require('./index');
+const objectArray = require('./index');
 
-xdescribe('All tests in this describe will be skipped. Remove the "x" from "describe" to turn them on', () => {
-    test('This test will be skipped', () => {
-        expect(true).toBe(true);
+const hasProperty = (dataType, obj) => {
+    return Object.keys(obj).some((key) => {
+        return typeof obj[key] === dataType
+    })
+}
+
+describe('There should be exported an array of ten objects that fullfill assignment requirements.', () => {
+    test('array has ten or more obejcts', () => {
+        expect(objectArray.length).toBeGreaterThanOrEqual(10);
     });
-});
+    test('each object should have 4 properties', () => {
+        for(let i = 0; i < objectArray.length; i++){
+            expect(Object.keys(objectArray[i]).length).toBeGreaterThanOrEqual(4)
+        }
+    });
+    test('each object should have one method', () => {
+        for(let i = 0; i < objectArray.length; i++){
+            expect(hasProperty("function", objectArray[i])).toBe(true);
+        }
+    });
+    test('each object should have one number as a property value', () => {
+        for(let i = 0; i < objectArray.length; i++){
+            expect(hasProperty("number", objectArray[i])).toBe(true);
+        }    
+    });
+    test('each object should have one string as a property value', () => {
+        for(let i = 0; i < objectArray.length; i++){
+            expect(hasProperty("string", objectArray[i])).toBe(true);
+        }    
+    });
+    test('each object should have one boolean as a property value', () => {
+        for(let i = 0; i < objectArray.length; i++){
+            expect(hasProperty("boolean", objectArray[i])).toBe(true);
+        }    
+    });
+})
+
