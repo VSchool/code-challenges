9bc52c282ba908afd15bb59abfe9284bf7343a22
Eric <ericnatejones@gmail.com>
2018-10-29 16:51:42 -0600

    optown function you up

--- daily-challenges/uptown-function-you-up/index.js
+++ daily-challenges/uptown-function-you-up/index.js
@@ -1,1 +1,19 @@
-//your function(s) here
+// your function(s) here
+
+var lyrics = ["This", "hit", "that", "ice", "cold", 
+              "Michelle", "Pfeiffer", "that", "white", 
+              "gold", "This", "one", "for", "them", 
+              "hood", "girls", "Them", "good", "girls", 
+              "straight", "masterpieces", "Stylin'", 
+              "whilen'", "livin'", "it", "up", "in", 
+              "the", "city", "Got", "Chucks", "on", 
+              "with", "Saint", "Laurent", "Gotta", "kiss", 
+              "myself", "I'm", "so", "pretty"];
+
+
+
+module.exports = {
+    getString,
+    reverse,
+    everyOther
+}
