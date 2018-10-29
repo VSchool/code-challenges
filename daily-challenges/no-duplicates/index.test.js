f5d9148bdacbe3427aea2f640840048683a90890
Bob Ziroll <elderziroll@gmail.com>
2018-10-29 17:06:24 -0600

    No dupes

--- daily-challenges/no-duplicates/index.test.js
+++ daily-challenges/no-duplicates/index.test.js
@@ -1,7 +1,22 @@
-const functionsToTest = require('./index');
+const {
+    noDuplicates
+} = require("./index")
 
-xdescribe('All tests in this describe will be skipped. Remove the "x" from "describe" to turn them on', () => {
-    test('This test will be skipped', () => {
-        expect(true).toBe(true);
-    });
-});
+describe("No duplicates", () => {
+    test("Duplicates are successfully removed", () => {
+        const str = "bookkeeper larry"
+        const expected = "bokepr lay"
+        const actual = noDuplicates(noDuplicates(str))
+        expect(expected).toBe(actual)
+    })
+    test("Function returns a string", () => {
+        const str = "bookkeeper larry"
+        const result = noDuplicates(noDuplicates(str))
+        expect(typeof result).toBe("string")
+    })
+    test("Input already with no duplicates returns same string", () => {
+        const str = "abc"
+        const result = noDuplicates(str)
+        expect(result).toBe(str)
+    })
+})
