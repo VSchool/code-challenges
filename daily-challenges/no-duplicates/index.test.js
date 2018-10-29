6536a81dbfecf93847ab21c92531196cc3ec5014
Bob Ziroll <elderziroll@gmail.com>
2018-10-29 17:11:58 -0600

    Fixed tests

--- daily-challenges/no-duplicates/index.test.js
+++ daily-challenges/no-duplicates/index.test.js
@@ -6,12 +6,13 @@ describe("No duplicates", () => {
     test("Duplicates are successfully removed", () => {
         const str = "bookkeeper larry"
         const expected = "bokepr lay"
-        const actual = noDuplicates(noDuplicates(str))
-        expect(expected).toBe(actual)
+        const actual = noDuplicates(str)
+        console.log(actual)
+        expect(actual).toBe(expected)
     })
     test("Function returns a string", () => {
         const str = "bookkeeper larry"
-        const result = noDuplicates(noDuplicates(str))
+        const result = noDuplicates(str)
         expect(typeof result).toBe("string")
     })
     test("Input already with no duplicates returns same string", () => {
