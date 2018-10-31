55e89002f52540581fe2684fe831b90032b81654
Eric <ericnatejones@gmail.com>
2018-10-31 11:33:21 -0600

    enemy generator

--- daily-challenges/enemy-generator/index.js
+++ daily-challenges/enemy-generator/index.js
@@ -1,1 +1,24 @@
 //your function(s) here
+
+function getEnemyType(){
+    
+}
+
+function getHitPoints(type){
+    
+}
+
+function getDefense(hitPoints){
+
+}
+
+function Enemy(){
+
+}
+
+function enemyGenerator(){
+    
+}
+
+
+module.exports = { enemyGenerator, Enemy, getDefense, getHitPoints, getEnemyType }
