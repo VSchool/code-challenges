261694ec7d1a89249ae1182b96945a619cddca3a
Bob Ziroll <elderziroll@gmail.com>
2018-11-05 12:26:28 -0700

    Updated readme, formatted test files

--- daily-challenges/array-methods-1/index.js
+++ daily-challenges/array-methods-1/index.js
@@ -1,9 +1,8 @@
-function sortedOfAge(arr){
+function sortedOfAge(arr) {
     // Return a list of everyone older than 18.
     // Have the new list be sorted by last name.
     // Return each name and age with <li>'s.
 }
-
 
 module.exports = {
     sortedOfAge
--- daily-challenges/array-methods-1/index.test.js
+++ daily-challenges/array-methods-1/index.test.js
@@ -1,46 +1,49 @@
 const { sortedOfAge } = require("./index")
-
 var peopleArray = [
     {
-      firstName: "Sarah",
-      lastName: "Palin",
-      age: 47
-    },{
-      firstName: "Frank",
-      lastName: "Zappa",
-      age: 12
-    },{
-      firstName: "Rick",
-      lastName: "Sanchez",
-      age: 78
-    },{
-      firstName: "Morty",
-      lastName: "Smith",
-      age: 29
-    },{
-      firstName: "Kyle",
-      lastName: "Mooney",
-      age: 27
-    },{
-      firstName: "Pasha",
-      lastName: "Datsyuk",
-      age: 13
-    },{
-      firstName: "Lev",
-      lastName: "Tolstoy",
-      age: 82
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
     }
-  ]
-
+]
 
 describe("Multiple array methods part 1", () => {
-
     describe("Sorted of age function", () => {
         const actual = sortedOfAge(peopleArray)
         const expected = [
             "<li>Kyle Mooney is 27</li>",
             "<li>Sarah Palin is 47</li>",
-            "<li>Rick Sanchez is 78</li>",  
+            "<li>Rick Sanchez is 78</li>",
             "<li>Morty Smith is 29</li>",
             "<li>Lev Tolstoy is 82</li>"
         ]
@@ -48,13 +51,12 @@ describe("Multiple array methods part 1", () => {
         test("should return an array", () => {
             expect(actual).toBeInstanceOf(Array)
         })
-    
+
         test("array returns expected result", () => {
             expect(actual).toEqual(expected)
         })
-    
     })
-    
+
     xdescribe("Extra credit challenges", () => {
         // Tests for extra credit challenges here
     })
