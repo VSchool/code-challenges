7dd1fd5e6f0768353cc4eb4f8412e3d6302ad43c
Bob Ziroll <elderziroll@gmail.com>
2018-11-06 17:30:49 -0700

    Updates

--- daily-challenges/es6-practice/index.js
+++ daily-challenges/es6-practice/index.js
@@ -1,100 +1,115 @@
-var name = 'John'
+var name = "John"
 var age = 101
 var pets = ["cat", "dog"]
-
 var petObjects = []
 
-function runForLoop(){
-  for (var i = 0; i < pets.length; i++){
-    var pet = {type: pets[i]}
-    if (pets[i] === "cat"){
-      var name = "fluffy"
-    } else {
-      var name = "spot"
+// let and const
+function runForLoop() {
+    for (var i = 0; i < pets.length; i++) {
+        var pet = { type: pets[i] }
+        if (pets[i] === "cat") {
+            var name = "fluffy"
+        } else {
+            var name = "spot"
+        }
+        pet.name = name
+        petObjects.push(pet)
     }
-    pet.name = name
-    petObjects.push(pet)
-  }
-  return name
+    return name
 }
 
+// Arrow functions
+// Task 1
 const carrots = ["bright orange", "ripe", "rotten"]
 
-function mapVegetables(arr){
-  return arr.map(function(carrot){
-    return {type: "carrot", name: carrot}
-  })
+function mapVegetables(arr) {
+    return arr.map(function(carrot) {
+        return { type: "carrot", name: carrot }
+    })
 }
 
+// Task 2
 const people = [
-  {
-      name: 'Princess Peach',
-      friendly: false
-  },
-  {
-      name: 'Luigi',
-      friendly: true
-  },
-  {
-      name: 'Mario',
-      friendly: true
-  },
-  {
-      name: 'Bowser',
-      friendly: false
-  }
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
 ]
 
-function filterForFriendly(arr){
-  return arr.filter(function(person){
-      return !!person.friendly
-  })
-
+function filterForFriendly(arr) {
+    return arr.filter(function(person) {
+        return !!person.friendly
+    })
 }
 
-function doMathSum(a, b){
-  return a + b
+// Task 3
+function doMathSum(a, b) {
+    return a + b
 }
 
-var produceProduct = function(a, b){
-  return a * b
+var produceProduct = function(a, b) {
+    return a * b
 }
 
+// Task 4
+function printString() {
+    // Your code here
+}
+
+// Task 5
 const animals = [
-  {
-      type: "dog",
-      name: "theodore"
-  },
-  {
-      type: "cat",
-      name: "whiskers"
-  },
-  {
-      type: "pig",
-      name: "piglette"
-  },
-  {
-      type: "dog",
-      name: "sparky"
-  }
-];
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+]
 
-function filterForDogs(arr){
- return arr.filter((animal)=>{
-   if (animal.type === "dog"){
-     return true
-   } else {
-     return false
-   }
- })
+function filterForDogs(arr) {
+    return arr.filter(animal => {
+        if (animal.type === "dog") {
+            return true
+        } else {
+            return false
+        }
+    })
 }
 
-function printString(firstName = "Jane", lastName = "Doe", age = 100){
-
+// Template Literals
+function printWelcome(name, location) {
+    // Your code here
 }
 
-function printWelcome(name, location){
- 
+module.exports = {
+    runForLoop,
+    filterForDogs,
+    filterForFriendly,
+    mapVegetables,
+    printString,
+    printWelcome,
+    doMathSum,
+    produceProduct
 }
-
-module.exports = {runForLoop, filterForDogs, filterForFriendly, mapVegetables, printString, printWelcome, doMathSum, produceProduct}
--- daily-challenges/es6-practice/index.test.js
+++ daily-challenges/es6-practice/index.test.js
@@ -1,79 +1,107 @@
-const {runForLoop, filterForDogs, filterForFriendly, mapVegetables, printString, printWelcome, doMathSum, produceProduct} = require("./index")
+const {
+    runForLoop,
+    filterForDogs,
+    filterForFriendly,
+    mapVegetables,
+    printString,
+    printWelcome,
+    doMathSum,
+    produceProduct
+} = require("./index")
 
-xdescribe("const and let", () => {
-    test("Function should return an 'John'", () => {	   
-        expect(runForLoop()).toBe("John")
+describe("ES6 Practice", () => {
+    describe("let and const", () => {
+        test("Function should return 'John'", () => {
+            expect(runForLoop()).toBe("John")
+        })
     })
-})
 
-describe("dog filter", () => {
-    test("Should only keep dogs", () => {	   
-        expect(filterForDogs([{type: "dog"}, {type: "cat"}])).toEqual([{type: "dog"}])
+    describe("dog filter", () => {
+        test("Should only keep dogs", () => {
+            expect(filterForDogs([{ type: "dog" }, { type: "cat" }])).toEqual([
+                { type: "dog" }
+            ])
+        })
     })
-})
 
-describe("add", () => {
-    test("Function should add two numbers", () => {	   
-        expect(doMathSum(12, 24)).toBe(26)
+    describe("add", () => {
+        test("should add two numbers", () => {
+            expect(doMathSum(2, 4)).toBe(6)
+        })
     })
-})
 
-describe("multipling", () => {
-    test("Should multiply two numbers", () => {	   
-        expect(produceProduct(4, 12)).toBe(48)
+    describe("multiply", () => {
+        test("should multiply two numbers", () => {
+            expect(produceProduct(4, 2)).toBe(8)
+        })
     })
-})
 
-describe("people filter", () => {
-    test("Should only keep friendlies", () => {	   
-        expect(filterForFriendly([
-            {
-                name: 'Princess Peach',
-                friendly: false
-            },
-            {
-                name: 'Luigi',
-                friendly: true
-            },
-            {
-                name: 'Mario',
-                friendly: true
-            },
-            {
-                name: 'Bowser',
-                friendly: false
-            }
-        ])).toEqual([{
-            name: 'Luigi',
-            friendly: true
-        },
-        {
-            name: 'Mario',
-            friendly: true
-        }])
+    describe("people filter", () => {
+        test("should only keep friendlies", () => {
+            const actual = filterForFriendly([
+                {
+                    name: "Princess Peach",
+                    friendly: false
+                },
+                {
+                    name: "Luigi",
+                    friendly: true
+                },
+                {
+                    name: "Mario",
+                    friendly: true
+                },
+                {
+                    name: "Bowser",
+                    friendly: false
+                }
+            ])
+            const expected = [
+                {
+                    name: "Luigi",
+                    friendly: true
+                },
+                {
+                    name: "Mario",
+                    friendly: true
+                }
+            ]
+            expect(actual).toEqual(expected)
+        })
     })
-})
 
-describe("map vegetables", () => {
-    test("Function should make array of objects out of array of strings", () => {	   
-        expect(mapVegetables(["bright orange", "ripe", "rotten"])).toEqual([ { type: 'carrot', name: 'bright orange' },
-        { type: 'carrot', name: 'ripe' },
-        { type: 'carrot', name: 'rotten' } ])
+    describe("map vegetables", () => {
+        test("should make array of objects out of array of strings", () => {
+            expect(mapVegetables(["bright orange", "ripe", "rotten"])).toEqual([
+                { type: "carrot", name: "bright orange" },
+                { type: "carrot", name: "ripe" },
+                { type: "carrot", name: "rotten" }
+            ])
+        })
     })
-})
 
-describe("Print string using defaults", () => {
-    test("Nothing will be passed into printString", () => {	   
-        expect(printString()).toBe("Hi Jane Doe, how does it feel to be 100?")
+    describe("Print string using defaults", () => {
+        test("Nothing will be passed into printString", () => {
+            expect(printString()).toBe(
+                "Hi Jane Doe, how does it feel to be 100?"
+            )
+        })
+
+        test("Something is passed into printString", () => {
+            expect(printString("Joe", "Schmoe", 30)).toBe(
+                "Hi Joe Schmoe, how does it feel to be 30?"
+            )
+        })
     })
-})
 
-describe("Print paragraph using template literals", () => {
-    test("Name and location will be variable", () => {	   
-        expect(printWelcome("Jason", "your worst nightmare")).toBe(`Hi Jason!
+    describe("Print paragraph using template literals", () => {
+        test("Name and location will be variable", () => {
+            expect(printWelcome("Jason", "your worst nightmare"))
+                .toBe(`Hi Jason!
 
 Welcome to your worst nightmare.
 
 I hope you enjoy your stay. Please ask the president of Hawaii if you need anything.`)
+        })
     })
 })
