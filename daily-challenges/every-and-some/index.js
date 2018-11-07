2903cdb9333090bd2be36519354a745cb9f7a5c8
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-06 18:50:55 -0700

    every-and-some

--- daily-challenges/every-and-some/index.js
+++ daily-challenges/every-and-some/index.js
@@ -1,1 +1,12 @@
-//your function(s) here
+function every(arr, callback){
+    // Your code here
+}
+
+function some(arr, callback){
+    // Your code here
+}
+
+module.exports = {
+    every,
+    some
+}
