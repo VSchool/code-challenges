c1597af135e7119ae2a2d55bf2b9c0b7f3310bf6
Bob Ziroll <elderziroll@gmail.com>
2018-11-08 10:28:44 -0700

    Fixed syntax error

--- daily-challenges/es6-practice-slope-style/index.js
+++ daily-challenges/es6-practice-slope-style/index.js
@@ -2,39 +2,54 @@
 // GREEN CIRCLE
 ///////////////
 
-function collectAnimals(/*your code here*/) {  
-    /*and here*/ 
+function collectAnimals(/*your code here*/) {
+    /*and here*/
 }
 
-// collectAnimals("dog", "cat", "mouse", "jackolope", "platypus"); 
+// collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
 
-function combineFruit(/*your code here*/){
-    return {/*your code here*/}
+function combineFruit(/*your code here*/) {
+    return {
+        /*your code here*/
+    }
 }
 
 // combineFruit(["apple", "pear"],
-             // ["cake", "pie"],
-             // ["carrit"])
+// ["cake", "pie"],
+// ["carrit"])
 
-function returnFirst(items){
-    const firstItem = items[0]; /*change this line to be es6*/
+function returnFirst(items) {
+    const firstItem = items[0] /*change this line to be es6*/
     return firstItem
 }
 
-const vacation = {  
+const vacation = {
     location: "Burly Idaho",
     duration: "2 weeks"
-};
-  
-function parseSentence(/*your code here*/){
+}
+
+function parseSentence(/*your code here*/) {
     return `We're going to have a good time in ${location} for ${duration}`
 }
 
-const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
+const favoriteActivities = [
+    "magnets",
+    "snowboarding",
+    "philanthropy",
+    "janitor work",
+    "eating"
+]
 
-function returnFavorites(arr){
+function returnFavorites(arr) {
     /*your code here*/
-    return "My top three favorite activities are: " + firstFav + ", " + secondFav + ", and " + thirdFav";
+    return (
+        "My top three favorite activities are: " +
+        firstFav +
+        ", " +
+        secondFav +
+        ", and " +
+        thirdFav
+    )
 }
 
 // returnFavorites(favoriteActivities)
@@ -43,15 +58,13 @@ function returnFavorites(arr){
 // BlUE SQUARE
 ///////////////
 
-function combineAnimals() {  
- 
-}
+function combineAnimals() {}
 
-const realAnimals = ["dog", "cat", "mouse"];
-const magicalAnimals = ["jackolope"];
-const mysteriousAnimals = ["platypus"];
+const realAnimals = ["dog", "cat", "mouse"]
+const magicalAnimals = ["jackolope"]
+const mysteriousAnimals = ["platypus"]
 
-combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals); 
+combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals)
 
 // ["dog", "cat", "mouse", "jackolope", "platypus"]
 
@@ -59,25 +72,25 @@ combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals);
 // BLACK DIAMOND
 ////////////////
 
-function product(a, b, c, d, e) {  
-    var numbers = [a,b,c,d,e];
-  
+function product(a, b, c, d, e) {
+    var numbers = [a, b, c, d, e]
+
     return numbers.reduce(function(acc, number) {
-        return acc * number;
+        return acc * number
     }, 1)
 }
 
-function unshift(array, a, b, c, d, e) {  
-    return [a, b, c, d, e].concat(array);
+function unshift(array, a, b, c, d, e) {
+    return [a, b, c, d, e].concat(array)
 }
 
 ///////////////
-// DOUBLE BLACK 
+// DOUBLE BLACK
 ///////////////
 
-function populatePeople(names){
-    return names.map(function(name){
-        name = name.split(" ");
+function populatePeople(names) {
+    return names.map(function(name) {
+        name = name.split(" ")
         // your code here
         return {
             firstName: firstName,
