656aef670628525ef8e7e19b48d813f0f7da8165
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-15 12:23:07 -0700

    no-way-a test-fix

--- exercises/no-way-a/index.test.js
+++ exercises/no-way-a/index.test.js
@@ -2,7 +2,7 @@ const { removeA } = require("./index");
 
 describe("RemoveA function", () => {
 
-    test("removes all characters 'a'in a given string", () => {
+    test("removes all characters 'a' and 'A' in a given string", () => {
         expect(removeA("absolutely")).toBe("bsolutely")
         expect(removeA("Always a good time")).toBe("lwys  good time")
     })
