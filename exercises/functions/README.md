b6f7c50de515cf822aaf20f7a99bdcf8807ca340
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-15 10:49:41 -0700

    functions-exercise

---
+++ exercises/functions/README.md
@@ -0,0 +1,14 @@
+Functions are important building blocks in any language. They help ensure that your code follows the DRY principle and that you follow a design principle called Separation of Concerns. Your functions should “do one thing and do it well” (McIlroy). These exercises will help strengthen your function know-how.
+
+- Write a function that accepts two numbers as parameters, and returns the sum.
+
+- Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
+
+- Write a function that accepts one number as a parameter, and returns whether that number is even or odd. (Return the string "even" or "odd");
+
+- Write a function that accepts a string as a parameter. If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). If the string is more than twenty characters long, return the first half of the string.
+
+#### Optional Challenge 
+- Write a function that accepts a number ‘n’ as a parameter. Then print the first ‘n’ Fibonacci numbers and return their sum.
+
+- Write a function that accepts a string as a parameter. Return the most frequently occuring letter in that string.
--- exercises/functions/index.js
+++ exercises/functions/index.js
@@ -1,1 +1,52 @@
-//your function(s) here
+// Write a function that accepts two numbers as parameters, and returns the sum.
+function sum(num1, num2){
+    // Your code here
+}
+
+// Write a function that accepts three numbers as parameters, 
+// and returns the largest of those numbers.
+function largestNum(num1, num2, num3){
+    // Your code here
+}
+
+// Write a function that accepts one number as a parameter, 
+// and returns whether that number is even or odd. (Return the string "even" or "odd");
+function evenOrOdd(num){
+    // Your code here
+}
+
+// Write a function that accepts a string as a parameter. 
+// If the length of the string is less than or equal to twenty characters long, 
+// return the string concatenated with itself (string + string). If the string 
+// is more than twenty characters long, return the first half of the string.
+function manipulateString(str){
+    // Your code here
+}
+
+
+// Optional Challenges //
+
+// Write a function that accepts a number ‘n’ as a parameter. 
+// Then print the first ‘n’ Fibonacci numbers and return their sum.
+function printFibonacciNum(n){
+
+}
+
+// Write a function that accepts a string as a parameter. 
+// Return the most frequently occuring letter in that string.
+// *White spaces count as a character*
+function mostFrequentLetter(string){
+
+}
+
+
+
+
+module.exports = {
+    sum,
+    largestNum,
+    evenOrOdd,
+    manipulateString,
+    printFibonacciNum,
+    mostFrequentLetter
+}
