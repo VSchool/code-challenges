3e55877c7f469374d580d6781b8f3c413e073c11
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-16 12:07:38 -0700

    loops-exercise

--- exercises/loops/index.js
+++ exercises/loops/index.js
@@ -1,1 +1,36 @@
-//your function(s) here
+// - Write a function that accepts a string as input. Write a loop and 
+//  add each letter of the string to an array. Then return the that new array.
+function addLettersToArray(string){
+    // Your code here
+}
+
+
+// - Write a function that accepts a string and a single character as inputs. 
+//   Write a loop that iterates over the string, and returns the position of the 
+//   first occurrence of the specified character. If the character is not found, tell that to the user
+//   by returning "Character not found!".
+function positionOfFirstOccurance(string, char){
+    // Your code here
+}
+
+
+// - Write a function that accepts an array of numbers as a parameter. 
+//   Return "Found 42!" when the number 42 is found, return "42 not found!" if it is not in the array.
+function find42(array){
+    // Your code here
+}
+
+
+// - Write a function that accepts 10 numbers in an array and then prints out 
+//   the smallest number out of the ten without using Math.min()
+function findTheSmallest(array){
+    // Your code here
+}
+
+
+module.exports = {
+    addLettersToArray,
+    positionOfFirstOccurance,
+    find42,
+    findTheSmallest
+}
