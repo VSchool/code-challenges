ce32b2fd997ec420de190f75ad8787dd088c7f58
Bob Ziroll <elderziroll@gmail.com>
2018-11-20 10:57:54 -0700

    Update index.js

--- daily-challenges/sorty-mcsortsort/index.js
+++ daily-challenges/sorty-mcsortsort/index.js
@@ -1,58 +1,6 @@
-const users = [
-    {
-        firstName: "Billy",
-        lastName: "Madison", 
-        age: 32
-    },
-    {
-        firstName: "Tommy",
-        lastName: "Pickles", 
-        age: 32
-    },
-    {
-        firstName: "Ray",
-        lastName: "Charles", 
-        age: 32
-    },
-    {
-        firstName: "Lady",
-        lastName: "Gaga", 
-        age: 32
-    },
-    {
-        firstName: "Elizabeth",
-        lastName: "Taylor", 
-        age: 32
-    },
-    {
-        firstName: "Led",
-        lastName: "Zeplin", 
-        age: 32
-    },
-    {
-        firstName: "Micael",
-        lastName: "Johnson", 
-        age: 32
-    },
-    {
-        firstName: "Keri",
-        lastName: "Strug", 
-        age: 32
-    },
-    {
-        firstName: "Chucky",
-        lastName: "Finster", 
-        age: 32
-    },
-    {
-        firstName: "Angelica",
-        lastName: "Pickles", 
-        age: 32
-    },
-]
-
 function sortyMcSortSort(arr, sortAscending){
     // Your code here
+    // See README.md for an example array
 }
 
 
