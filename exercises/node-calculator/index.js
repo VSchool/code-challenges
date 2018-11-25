cabef695f5066e7535072e2093c767ed7da57569
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-25 11:55:08 -0700

    node-calculator

--- exercises/node-calculator/index.js
+++ exercises/node-calculator/index.js
@@ -1,1 +1,25 @@
-//your function(s) here
+// install and require readline-sync
+
+function add(num1, num2){
+    // Your code here
+}
+
+function sub(num1, num2){
+    // Your code here
+}
+
+function mul(num1, num2){
+    // Your code here
+}
+
+function div(num1, num2){
+    // Your code here
+}
+
+
+module.exports = {
+    add,
+    sub,
+    mul,
+    div
+}
--- exercises/node-calculator/index.test.js
+++ exercises/node-calculator/index.test.js
@@ -1,7 +1,29 @@
-const functionsToTest = require('./index');
+const { add, sub, mul, div} = require("./index");
 
-xdescribe('All tests in this describe will be skipped. Remove the "x" from "describe" to turn them on', () => {
-    test('This test will be skipped', () => {
-        expect(true).toBe(true);
-    });
-});
+describe("Node Calculator", () => {
+   
+    describe("add function", () => {
+        test("adds two numbers and returns the sum", () => {
+            expect(add(3, 5)).toBe(8)
+        })
+    })
+
+    describe("sub function", () => {
+        test("sutracts num2 from num1 and returns the sum", () => {
+            expect(sub(10, 3)).toBe(7)
+        })
+    })
+
+    describe("mul function", () => {
+        test("multiplies two numbers together and returns the sum", () => {
+            expect(mul(20, 4)).toBe(80)
+        })
+    })
+
+    describe("div function", () => {
+        test("divides num1 by num2 and returns the sum", () => {
+            expect(div(8, 4)).toBe(2)
+        })
+    })
+
+})
