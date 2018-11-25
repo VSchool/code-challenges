c05c10a4dd3f6d53fad4889c68e89b628d856610
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-25 13:06:08 -0700

    problem-solving

---
+++ exercises/problem-solving/README.md
@@ -0,0 +1,20 @@
+Write a function that takes an array of numbers and returns the largest (without using `Math.max()`)
+
+```js
+largest([3, 5, 2, 8, 1]) // => 8
+```
+
+Write a function that takes an array of words and a character and return each word that has that letter present.
+
+```js
+lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!") // => ["C%4!", "Hey!"]
+```
+
+Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+
+```js
+isDivisible(4, 2) // => true
+isDivisible(9, 3) // => true
+isDivisible(15, 4) // => false
+```
+
--- exercises/problem-solving/index.js
+++ exercises/problem-solving/index.js
@@ -1,1 +1,26 @@
-//your function(s) here
+// Write a function that takes an array of numbers and returns the largest 
+// (without using `Math.max()`)
+function largest(array){
+    // Your code here
+}
+
+
+// Write a function that takes an array of words and a character and return 
+// each word that has that letter present.
+function lettersWithStrings(array, char){
+    // Your code here
+}
+
+
+// Write a function that takes a num1 and num2 and returns 
+// whether num1 is divisible by num2.
+function isDivisible(num1, num2){
+    // Your code here
+}
+
+
+module.exports = {
+    largest,
+    lettersWithStrings,
+    isDivisible
+}
