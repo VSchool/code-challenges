0eeb2834b32548e2d680b3f44e876f66f263e548
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-26 09:08:46 -0700

    map-exercise

--- exercises/map/index.js
+++ exercises/map/index.js
@@ -1,1 +1,38 @@
-//your function(s) here
+///Make an array of numbers that are doubles of the first array
+function doubleNumbers(arr){
+
+}
+
+///Take an array of numbers and make them strings
+function stringItUp(arr){
+
+}
+
+// Capitalize each of an array of names
+function capitalizeNames(arr){
+
+}
+
+// Make an array of strings of the names
+function namesOnly(arr){
+
+}
+// Make an array of strings of the names saying whether or not they can go to The Matrix
+function makeStrings(arr){
+
+}
+
+// Make an array of the names in h1s, and the ages in h2s
+function readyToPutInTheDOM(arr){
+
+}
+
+
+module.exports = {
+    doubleNumbers,
+    stringItUp,
+    capitalizeNames,
+    namesOnly,
+    makeStrings,
+    readyToPutInTheDOM
+}
