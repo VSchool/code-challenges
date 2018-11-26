88bd7dd4278346181e33524b4db9a599a60573f8
Nate Jensen <nathan.jensen.58@gmail.com>
2018-11-26 13:22:42 -0700

    sort-exercise

--- exercises/sort/index.js
+++ exercises/sort/index.js
@@ -1,1 +1,32 @@
-//your function(s) here
+// Sort an array from smallest number to largest
+function leastToGreatest(arr) {
+
+}
+
+// Sort an array from largest number to smallest
+function greatestToLeast(arr) {
+
+}
+
+// Sort an array from shortest string to longest
+function lengthSort(arr) {
+
+}
+
+// Sort an array alphabetically
+function alphabetical(arr) {
+
+}
+
+// Sort the objects in the array by age
+function byAge(arr){
+
+}
+
+module.exports = {
+    leastToGreatest,
+    greatestToLeast,
+    lengthSort,
+    alphabetical,
+    byAge
+}
--- exercises/sort/index.test.js
+++ exercises/sort/index.test.js
@@ -1,7 +1,38 @@
-const functionsToTest = require('./index');
+const {
+    leastToGreatest,
+    greatestToLeast,
+    lengthSort,
+    alphabetical,
+    byAge
+} = require("./index");
 
-xdescribe('All tests in this describe will be skipped. Remove the "x" from "describe" to turn them on', () => {
-    test('This test will be skipped', () => {
-        expect(true).toBe(true);
-    });
-});
+describe("Array Sort Exercise", () => {
+   
+    describe("Least to Greatest function", () => {
+        test("returns array sorted from least to greatest", () => {
+            expect(leastToGreatest([1, 3, 5, 2, 90, 20])).toEqual([1, 2, 3, 5, 20, 90])
+        })
+    })
+
+    describe("Greatest to Least function", () => {
+        test("returns array sorted from greatest to least", () => {
+            expect(greatestToLeast([1, 3, 5, 2, 90, 20])).toEqual([90, 20, 5, 3, 2, 1])
+        })
+    })
+
+    describe("Length Sort function", () => {
+        test("returns array of strings sorted least to greatest in length", () => {
+            // Your tests here
+        })
+    })
+
+    // Remove the 'x' to have Jest not skip the 'describe'
+    xdescribe("Alphabetical function", () => {
+        // Your tests here
+    })
+
+    xdescribe("By Age function", () => {
+        // Your tests here
+    })
+
+})
